2cf4160bb952b80f48052f8b59e04c338f27db47
MiSo_home <jimrayder3701@gmail.com>
2023-09-03 14:41:10 +0900

    feat: Swagger 적용

---
+++ suldaksuldak-core/src/main/java/com/sulsul/suldaksuldak/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.sulsul.suldaksuldak.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(true)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.sulsul.suldaksuldak.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("술닥술닥 API")
+                .description("사전예약 API / 메인 API")
+                .version("0.1")
+                .build();
+    }
+}
