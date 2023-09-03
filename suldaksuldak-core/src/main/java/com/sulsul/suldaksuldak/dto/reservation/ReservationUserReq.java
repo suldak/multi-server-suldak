4184661b49a1aac6a96cf866047ebae05ac5070d
MiSo_home <jimrayder3701@gmail.com>
2023-09-03 15:11:07 +0900

    style: 사전예약 api에 swagger 적용

--- suldaksuldak-core/src/main/java/com/sulsul/suldaksuldak/dto/reservation/ReservationUserReq.java
+++ suldaksuldak-core/src/main/java/com/sulsul/suldaksuldak/dto/reservation/ReservationUserReq.java
@@ -1,5 +1,7 @@
 package com.sulsul.suldaksuldak.dto.reservation;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 @Setter
@@ -8,7 +10,9 @@ import lombok.*;
 @EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel(value = "사전예약 User Req")
 public class ReservationUserReq {
+    @ApiModelProperty(value = "사용자 email", required = true)
     String userEmail;
 
     public ReservationUserDto toDto() {
--- suldaksuldak-reservation/src/main/java/com/sulsul/suldaksuldak/controller/reservation/ReservationController.java
+++ suldaksuldak-reservation/src/main/java/com/sulsul/suldaksuldak/controller/reservation/ReservationController.java
@@ -7,6 +7,10 @@ import com.sulsul.suldaksuldak.dto.reservation.ReservationUserReq;
 import com.sulsul.suldaksuldak.dto.reservation.ReservationUserRes;
 import com.sulsul.suldaksuldak.exception.GeneralException;
 import com.sulsul.suldaksuldak.service.reservation.ReservationService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -18,9 +22,19 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/api/reservation")
+@Api(tags = "[사전예약] ReservationController")
 public class ReservationController {
     private final ReservationService reservationService;
 
+    @ApiOperation(
+            value = "사전예약 User 등록",
+            notes = "User를 등록합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 10000, message = "이메일이 공백이거나 이미 등록된 이메일일 경우"),
+            @ApiResponse(code = 20002, message = "시스템 Error")
+    })
     @PostMapping(value = "/user")
     public ApiDataResponse<Boolean> createUser(
             @RequestBody ReservationUserReq reservationUserReq
@@ -32,6 +46,14 @@ public class ReservationController {
         );
     }
 
+    @ApiOperation(
+            value = "사전예약 User 목록 반환",
+            notes = "사전예약 완료한 User의 목록을 반환합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 20002, message = "시스템 Error")
+    })
     @GetMapping(value = "/user")
     public ApiDataResponse<List<ReservationUserRes>> getAllUserList() {
         return ApiDataResponse.of(
@@ -42,6 +64,15 @@ public class ReservationController {
         );
     }
 
+    @ApiOperation(
+            value = "UserEmail에 해당하는 User 반환",
+            notes = "UserEmail이 같은 User를 반환합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 10000, message = "이메일이 공백"),
+            @ApiResponse(code = 20002, message = "시스템 Error")
+    })
     @GetMapping(value = "/user-detail")
     public ApiDataResponse<ReservationUserRes> getUserDetail(
             String userEmail
