9eeda3875de9f29af4c7bc05898414cebfa77cbe
MiSo_home <jimrayder3701@gmail.com>
2023-09-05 21:27:46 +0900

    feat: Admin Server 셋팅

---
+++ suldaksuldak-admin/src/main/java/com/sulsul/suldaksuldak/SuldakAdminApplication.java
@@ -0,0 +1,20 @@
+package com.sulsul.suldaksuldak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.util.Locale;
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class SuldakAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SuldakAdminApplication.class, args);
+    }
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        Locale.setDefault(Locale.KOREA);
+    }
+}
