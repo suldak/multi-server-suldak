fffff7c536c16301bdf03f6a150489de029af391
ITF_MiSo <jimrayder3701@gmail.com>
2023-09-13 17:40:09 +0900

    feat: 술 관련 정보 추가 Controller, Service 구현 (술, 레시피)

---
+++ suldaksuldak-admin/src/main/java/com/sulsul/suldaksuldak/Service/liquor/LiquorAddService.java
@@ -0,0 +1,88 @@
+package com.sulsul.suldaksuldak.Service.liquor;
+
+import com.sulsul.suldaksuldak.constant.error.ErrorCode;
+import com.sulsul.suldaksuldak.domain.liquor.Liquor;
+import com.sulsul.suldaksuldak.dto.liquor.liquor.LiquorDto;
+import com.sulsul.suldaksuldak.dto.liquor.recipe.LiquorRecipeDto;
+import com.sulsul.suldaksuldak.exception.GeneralException;
+import com.sulsul.suldaksuldak.repo.liquor.liquor.LiquorRepository;
+import com.sulsul.suldaksuldak.repo.liquor.recipe.LiquorRecipeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class LiquorAddService {
+    private final LiquorRepository liquorRepository;
+    private final LiquorRecipeRepository liquorRecipeRepository;
+
+    /**
+     * 술 생성 및 수정
+     */
+    public Boolean createLiquor(
+            LiquorDto liquorDto
+    ) {
+        try {
+            if (liquorDto.getId() == null) {
+                liquorRepository.save(liquorDto.toEntity());
+            } else {
+                liquorRepository.findById(liquorDto.getId())
+                        .ifPresentOrElse(
+                                findEntity -> {
+                                    liquorRepository.save(liquorDto.updateEntity(findEntity));
+                                },
+                                () -> {
+                                    throw new GeneralException(ErrorCode.NOT_FOUND, "NOT FOUND Liquor DATA");
+                                }
+                        );
+            }
+        } catch (GeneralException e) {
+            throw new GeneralException(e.getErrorCode(), e.getMessage());
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e.getMessage());
+        }
+        return true;
+    }
+
+    /**
+     * 술의 레피시 저장
+     */
+    public Boolean createLiquorRecipe(
+            LiquorRecipeDto liquorRecipeDto
+    ) {
+        try {
+            if (liquorRecipeDto.getLiquorId() == null) {
+                throw new GeneralException(ErrorCode.BAD_REQUEST, "NOT FOUND Liquor PRI KEY");
+            }
+            Optional<Liquor> liquorOptional = liquorRepository.findById(liquorRecipeDto.getLiquorId());
+            if (liquorOptional.isEmpty()) {
+                throw new GeneralException(ErrorCode.NOT_FOUND, "NOT FOUND Liquor DATA");
+            }
+            if (liquorRecipeDto.getId() == null) {
+                liquorRecipeRepository.save(liquorRecipeDto.toEntity(liquorOptional.get()));
+            } else {
+                liquorRecipeRepository.findById(liquorRecipeDto.getId())
+                        .ifPresentOrElse(
+                                findEntity -> {
+                                    liquorRecipeRepository.save(
+                                            liquorRecipeDto.updateEntity(findEntity)
+                                    );
+                                },
+                                () -> {
+                                    throw new GeneralException(ErrorCode.NOT_FOUND, "NOT FOUND RECIPE DATA");
+                                }
+                        );
+            }
+        } catch (GeneralException e) {
+            throw new GeneralException(e.getErrorCode(), e.getMessage());
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e.getMessage());
+        }
+        return true;
+    }
+
+}
---
+++ suldaksuldak-admin/src/main/java/com/sulsul/suldaksuldak/controller/liquor/LiquorAddController.java
@@ -0,0 +1,49 @@
+package com.sulsul.suldaksuldak.controller.liquor;
+
+import com.sulsul.suldaksuldak.Service.liquor.LiquorAddService;
+import com.sulsul.suldaksuldak.dto.ApiDataResponse;
+import com.sulsul.suldaksuldak.dto.liquor.liquor.LiquorReq;
+import com.sulsul.suldaksuldak.dto.liquor.recipe.LiquorRecipeReq;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/api/admin/liquor/add")
+@Api(tags = "[ADMIN] 술 관련 정보 추가")
+public class LiquorAddController {
+    private final LiquorAddService liquorAddService;
+
+    @ApiOperation(
+            value = "술 저장",
+            notes = "술 데이터를 생성하거나 수정합니다."
+    )
+    @PostMapping(value = "/liquor")
+    public ApiDataResponse<Boolean> createLiquor(
+            @RequestBody LiquorReq liquorReq
+    ) {
+        return ApiDataResponse.of(
+                liquorAddService.createLiquor(liquorReq.toDto())
+        );
+    }
+
+    @ApiOperation(
+            value = "술의 레시피 저장",
+            notes = "술의 레시피를 생성하거나 수정합니다."
+    )
+    @PostMapping(value = "/liquor-recipe")
+    public ApiDataResponse<Boolean> createLiquorRecipe(
+            @RequestBody LiquorRecipeReq liquorRecipeReq
+    ) {
+        return ApiDataResponse.of(
+                liquorAddService.createLiquorRecipe(liquorRecipeReq.toDto())
+        );
+    }
+}
