4ec1e2ea28b365f1baed58ab2602c86c82d6a785
ITF_Jh <jh.heo@itforest.net>
2023-11-01 15:40:22 +0900

    feat: AdminAuth Controller, Service 구현

---
+++ suldaksuldak-admin/src/main/java/com/sulsul/suldaksuldak/Service/auth/AdminAuthService.java
@@ -0,0 +1,102 @@
+package com.sulsul.suldaksuldak.Service.auth;
+
+import com.sulsul.suldaksuldak.constant.error.ErrorCode;
+import com.sulsul.suldaksuldak.constant.error.ErrorMessage;
+import com.sulsul.suldaksuldak.dto.auth.AdminUserDto;
+import com.sulsul.suldaksuldak.exception.GeneralException;
+import com.sulsul.suldaksuldak.repo.admin.auth.AdminUserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class AdminAuthService {
+    private final AdminUserRepository adminUserRepository;
+
+    public Optional<AdminUserDto> loginAdminUser(
+            String adminId,
+            String adminPw
+    ) {
+        try {
+            return adminUserRepository.doLogin(
+                    adminId,
+                    adminPw
+            );
+        } catch (GeneralException e) {
+            throw new GeneralException(e.getErrorCode(), e.getMessage());
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e.getMessage());
+        }
+    }
+
+    public Boolean createAdminUser(
+            AdminUserDto adminUserDto
+    ) {
+        try {
+            List<AdminUserDto> idList = adminUserRepository.findByUserId(adminUserDto.getAdminId());
+            if (adminUserDto.getId() == null) {
+                if (!idList.isEmpty()) {
+                    throw new GeneralException(ErrorCode.BAD_REQUEST, "ID가 중복됩니다.");
+                }
+                adminUserRepository.save(
+                        adminUserDto.toEntity()
+                );
+            } else {
+                adminUserRepository.findById(adminUserDto.getId())
+                        .ifPresentOrElse(
+                                findEntity -> {
+                                    if (!findEntity.getAdminId().equals(adminUserDto.getAdminId())) {
+                                        if (!idList.isEmpty()) {
+                                            throw new GeneralException(ErrorCode.BAD_REQUEST, "ID가 중복됩니다.");
+                                        }
+                                    }
+                                    adminUserRepository.save(
+                                            adminUserDto.updateEntity(findEntity)
+                                    );
+                                },
+                                () -> {
+                                    throw new GeneralException(
+                                            ErrorCode.NOT_FOUND,
+                                            ErrorMessage.NOT_FOUND_USER
+                                    );
+                                }
+                        );
+            }
+        } catch (GeneralException e) {
+            throw new GeneralException(e.getErrorCode(), e.getMessage());
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e.getMessage());
+        }
+        return true;
+    }
+
+    public List<AdminUserDto> getAllAdminUser() {
+        try {
+            return adminUserRepository.findAllAdminUser();
+        } catch (GeneralException e) {
+            throw new GeneralException(e.getErrorCode(), e.getMessage());
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e.getMessage());
+        }
+    }
+
+    public Boolean deleteAdminUser(
+            Long priKey
+    ) {
+        try {
+            if (priKey == null)
+                throw new GeneralException(ErrorCode.BAD_REQUEST, "ID NULL");
+            adminUserRepository.deleteById(priKey);
+        } catch (GeneralException e) {
+            throw new GeneralException(e.getErrorCode(), e.getMessage());
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.DATA_ACCESS_ERROR, e.getMessage());
+        }
+        return true;
+    }
+}
---
+++ suldaksuldak-admin/src/main/java/com/sulsul/suldaksuldak/controller/auth/AdminAuthController.java
@@ -0,0 +1,131 @@
+package com.sulsul.suldaksuldak.controller.auth;
+
+import com.sulsul.suldaksuldak.Service.auth.AdminAuthService;
+import com.sulsul.suldaksuldak.constant.error.ErrorCode;
+import com.sulsul.suldaksuldak.dto.ApiDataResponse;
+import com.sulsul.suldaksuldak.dto.auth.AdminUserDto;
+import com.sulsul.suldaksuldak.dto.auth.AdminUserRes;
+import com.sulsul.suldaksuldak.dto.auth.TokenRes;
+import com.sulsul.suldaksuldak.exception.GeneralException;
+import com.sulsul.suldaksuldak.tool.TokenUtils;
+import com.sulsul.suldaksuldak.tool.UtilTool;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/api/admin/auth")
+@Api(tags = "[ADMIN] 관리자 로그인 및 토큰 관리")
+public class AdminAuthController {
+    private final AdminAuthService adminAuthService;
+
+    @ApiOperation(
+            value = "관리자 로그인",
+            notes = "관리자 페이지 로그인"
+    )
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "adminId", value = "관리자 ID", required = true, dataTypeClass = String.class),
+            @ApiImplicitParam(name = "adminPw", value = "관리자 PW", required = true, dataTypeClass = String.class)
+    })
+    @PostMapping(value = "/login")
+    public ApiDataResponse<AdminUserRes> adminLogin(
+            String adminId,
+            String adminPw
+    ) {
+        try {
+            String encryptedPw = UtilTool.encryptPassword(adminPw, adminId);
+            Optional<AdminUserDto> adminUserDto =
+                    adminAuthService.loginAdminUser(adminId, encryptedPw);
+            if (adminUserDto.isEmpty()) {
+                throw new GeneralException(ErrorCode.NOT_FOUND, "ID 또는 PW가 없습니다.");
+            }
+            return ApiDataResponse.of(
+                    AdminUserRes.from(
+                            adminUserDto.get(),
+                            TokenRes.from(TokenUtils.getTokenMap(adminUserDto.get().changeDto()))
+                    )
+            );
+        } catch (GeneralException e) {
+            throw new GeneralException(e.getErrorCode(), e.getMessage());
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.INTERNAL_ERROR, e.getMessage());
+        }
+    }
+
+    @ApiOperation(
+            value = "관리자 생성 및 수정",
+            notes = "관리자 계정의 정보를 생성하거나 수정합니다."
+    )
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "priKey", value = "관리자 계정 기본키 (없으면 생성)", dataTypeClass = Long.class),
+            @ApiImplicitParam(name = "adminId", value = "관리자 ID", required = true, dataTypeClass = String.class),
+            @ApiImplicitParam(name = "adminPw", value = "관리자 PW", required = true, dataTypeClass = String.class),
+            @ApiImplicitParam(name = "adminNm", value = "관리자 이름", required = true, dataTypeClass = String.class)
+    })
+    @PostMapping(value = "/signup")
+    public ApiDataResponse<Boolean> signUpAdminUser(
+            Long priKey,
+            String adminId,
+            String adminPw,
+            String adminNm
+    ) {
+        try {
+            String encryptedPw = UtilTool.encryptPassword(adminPw, adminId);
+            return ApiDataResponse.of(
+                    adminAuthService.createAdminUser(
+                            AdminUserDto.of(
+                                    priKey,
+                                    adminId,
+                                    encryptedPw,
+                                    adminNm
+                            )
+                    )
+            );
+        } catch (GeneralException e) {
+            throw new GeneralException(e.getErrorCode(), e.getMessage());
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.INTERNAL_ERROR, e.getMessage());
+        }
+    }
+
+    @ApiOperation(
+            value = "관리자 목록 조회",
+            notes = "관리자 목록을 조회합니다."
+    )
+    @GetMapping("/admin-user")
+    public ApiDataResponse<List<AdminUserRes>> getAllAdminUser() {
+        return ApiDataResponse.of(
+                adminAuthService.getAllAdminUser()
+                        .stream()
+                        .map(AdminUserRes::from)
+                        .toList()
+        );
+    }
+
+    @ApiOperation(
+            value = "관리자 삭제",
+            notes = "관리자 계정을 삭제 합니다."
+    )
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "priKey", value = "관리자 계정 기본키", required = true, dataTypeClass = Long.class)
+    })
+    @DeleteMapping(value = "/admin-user")
+    public ApiDataResponse<Boolean> deleteAdminUser(
+            Long priKey
+    ) {
+        return ApiDataResponse.of(
+                adminAuthService.deleteAdminUser(
+                        priKey
+                )
+        );
+    }
+}
