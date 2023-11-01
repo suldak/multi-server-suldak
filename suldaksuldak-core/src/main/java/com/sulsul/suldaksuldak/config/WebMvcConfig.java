ce908398aa0512c267336fafa2f7dc4abef4e4a9
ITF_Jh <jh.heo@itforest.net>
2023-11-01 16:33:55 +0900

    fix: CORS 에러 처리

--- suldaksuldak-core/src/main/java/com/sulsul/suldaksuldak/config/WebMvcConfig.java
+++ suldaksuldak-core/src/main/java/com/sulsul/suldaksuldak/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.sulsul.suldaksuldak.config;
 
 import com.sulsul.suldaksuldak.interceptor.Interceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +12,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new Interceptor())
                 .excludePathPatterns("/css/**", "/images/**", "/js/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
