1b73827cd0aca6322d114e444b6b7f3a9942793f
MiSo_home <jimrayder3701@gmail.com>
2024-02-27 21:55:54 +0900

    style: 모임 피드백 조회 API에 Swagger 구현

--- suldaksuldak-admin/src/main/java/com/sulsul/suldaksuldak/controller/party/PartyFeedbackViewController.java
+++ suldaksuldak-admin/src/main/java/com/sulsul/suldaksuldak/controller/party/PartyFeedbackViewController.java
@@ -6,6 +6,9 @@ import com.sulsul.suldaksuldak.dto.ApiDataResponse;
 import com.sulsul.suldaksuldak.dto.admin.feedback.UserPartyFeedbackRes;
 import com.sulsul.suldaksuldak.service.party.PartyFeedbackViewService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +26,19 @@ import java.util.List;
 public class PartyFeedbackViewController {
     private final PartyFeedbackViewService partyFeedbackViewService;
 
+    @ApiOperation(
+            value = "모임 피드백 목록을 조회합니다.",
+            notes = "유저가 즐겨찾기를 많이 등록한 술 순서대로 목록을 조회합니다. (Pageable)"
+    )
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "comment", value = "모임 피드백 상세 내용", dataTypeClass = String.class),
+            @ApiImplicitParam(name = "searchStartTime", value = "검색 시작 일시 (yyyy-MM-dd'T'HH:mm:ss)", dataTypeClass = String.class, example = "2023-10-05T00:00:00"),
+            @ApiImplicitParam(name = "searchEndTime", value = "검색 끝 일시 (yyyy-MM-dd'T'HH:mm:ss)", dataTypeClass = String.class, example = "2023-10-06T00:00:00"),
+            @ApiImplicitParam(name = "feedbackType", value = "모임 피드백 타입"),
+            @ApiImplicitParam(name = "partyPriKey", value = "검색 하고 싶은 모임의 기본키", dataTypeClass = Long.class),
+            @ApiImplicitParam(name = "writerPriKey", value = "피드백을 남김 유저의 기본키", dataTypeClass = Long.class),
+            @ApiImplicitParam(name = "targetUserPriKey", value = "피드백을 받은 유저의 기본키", dataTypeClass = Long.class),
+    })
     @GetMapping("/list")
     public ApiDataResponse<List<UserPartyFeedbackRes>> getUserPartyFeedbackList(
             FeedbackType feedbackType,
@@ -30,8 +46,8 @@ public class PartyFeedbackViewController {
             Long partyPriKey,
             Long writerPriKey,
             Long targetUserPriKey,
-            LocalDateTime startAt,
-            LocalDateTime endAt
+            LocalDateTime searchStartTime,
+            LocalDateTime searchEndTime
     ) {
         return ApiDataResponse.of(
                 partyFeedbackViewService.getUserPartyFeedbackDtoList(
@@ -40,8 +56,8 @@ public class PartyFeedbackViewController {
                         partyPriKey,
                         writerPriKey,
                         targetUserPriKey,
-                        startAt,
-                        endAt
+                        searchStartTime,
+                        searchEndTime
                 ).stream().map(UserPartyFeedbackRes::from).toList()
         );
     }
