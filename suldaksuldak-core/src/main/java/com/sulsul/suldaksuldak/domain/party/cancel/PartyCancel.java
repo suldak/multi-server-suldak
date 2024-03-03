ee4c8611109d3562d97784131382d4d7aff74443
MiSo_home <jimrayder3701@gmail.com>
2024-03-03 22:02:02 +0900

    TODO: PartyCancel

---
+++ suldaksuldak-core/src/main/java/com/sulsul/suldaksuldak/domain/party/cancel/PartyCancel.java
@@ -0,0 +1,20 @@
+//package com.sulsul.suldaksuldak.domain.party.cancel;
+//
+//import lombok.Getter;
+//import lombok.ToString;
+//
+//import javax.persistence.*;
+//
+//@Getter
+//@ToString
+//@Table(
+//        name = "tb_party_cancel"
+//)
+//@EntityListeners(AutoCloseable.class)
+//@Entity(name = "partyCancel")
+//public class PartyCancel {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(columnDefinition = "BIGINT UNSIGNED")
+//    private Long id;
+//}
